4f9b478614a4ea20450627e377537ad546a86ae9
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-05 17:49:54 -0300

    ✨ feat: notification service and toast message

---
+++ front-end/src/app/services/notification.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  constructor(private toastService: HotToastService) {}
+  showMessage(msg: string) {
+    this.toastService.success(msg)
+  }
+}
