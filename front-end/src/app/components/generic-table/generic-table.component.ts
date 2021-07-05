2f3c5fefb6fb4e349a06a9f2219ceb2bedfa4c61
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-05 16:58:17 -0300

    ✨ feat: generic table component

---
+++ front-end/src/app/components/generic-table/generic-table.component.ts
@@ -0,0 +1,59 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ActionEvent, ButtonAction } from 'src/app/models/actions';
+
+@Component({
+  selector: 'app-generic-table',
+  templateUrl: './generic-table.component.html',
+  styleUrls: ['./generic-table.component.scss'],
+})
+export class GenericTableComponent<T> {
+  @Input() hasDefaultActions = true;
+  @Input() tableData: T[];
+  @Output() actionEvent = new EventEmitter<ActionEvent<T>>();
+  readonly defaultActions: ButtonAction[] = [
+    {
+      buttonText: 'Editar',
+      eventName: 'edit',
+      iconName: 'edit',
+    },
+    {
+      buttonText: 'Deletar',
+      eventName: 'delete',
+      iconName: 'delete',
+    },
+  ];
+
+  @Input()
+  set itemsDisplayedColumns(items: any) {
+    this._itemsDisplayedColumns = {
+      ...items,
+      actions: items['actions'] ?? 'Ações',
+    };
+  }
+  get itemsDisplayedColumns() {
+    return this._itemsDisplayedColumns;
+  }
+
+  @Input()
+  set actions(items: ButtonAction[]) {
+    this._actions = items;
+  }
+  get actions(): ButtonAction[] {
+    const actions = [
+      ...((this.hasDefaultActions && this.defaultActions) || []),
+      ...(this._actions || []),
+    ];
+    return actions;
+  }
+
+  get columnsWithoutActions(): string[] {
+    return this.objectKeys(this.itemsDisplayedColumns).filter(
+      (c) => c !== 'actions'
+    );
+  }
+
+  private _actions: ButtonAction[] = [];
+  private _itemsDisplayedColumns: any;
+
+  objectKeys = Object.keys;
+}
