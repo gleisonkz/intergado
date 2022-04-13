576e9e3f8910d4c7b5c69c5eb341ced8ddf25d6b
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-13 01:17:38 -0300

    🧪 test: add test case for add an animal

--- front-end/cypress/integration/home.spec.ts
+++ front-end/cypress/integration/home.spec.ts
@@ -1,15 +1,31 @@
 import { NO_ANIMALS_MESSAGE } from '@gd/models';
 
-describe('My First Test', () => {
-  it('Should have the text "Intergados" on header', () => {
-    cy.clearLocalStorage();
+describe('[Page] - Home Page', () => {
+  it('should have the text "Intergados" on header', () => {
     cy.visit('/');
     cy.get('[data-testid="header-title"]').should('contain', 'Intergados');
   });
 
-  it('Should have a default no-data message when no data is loaded', () => {
-    cy.clearLocalStorage();
+  it('should have a default no-data message when no data is loaded', () => {
     cy.visit('/');
     cy.get('[data-testid="generic-table-no-data"]').should('contain', NO_ANIMALS_MESSAGE);
+  });
+
+  it('should be able to add an animal', () => {
+    cy.visit('/');
+    cy.get('[data-testid="add-button"]').click();
+
+    const EXPECTED_MANEJO = 'Manejo';
+    const EXPECTED_TAG = 'Tag';
+
+    cy.get('[data-testid="manejo-input"]').type(EXPECTED_MANEJO);
+    cy.get('[data-testid="tag-input"]').type(EXPECTED_TAG);
+
+    cy.get('[data-testid="save-button"]').click();
+
+    cy.get('[data-testid="generic-table-no-data"]').should('not.exist');
+    cy.get('[data-testid="table-row"]').should('have.length', 1);
+
+    cy.get('[data-testid="table-row"]').should('contain', EXPECTED_MANEJO).should('contain', EXPECTED_TAG);
   });
 });
